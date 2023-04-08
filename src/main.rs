687891f89ae7f9371607d2c284560e0ffed9d7c3
Paul Cortes <paulkerg@gmail.com>
2023-04-07 23:22:28 -0500

    calculator basic operations

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use regex::Regex;
+
+fn math_operation(reg: Regex, mut operation: String, operator: &str) -> String {
+  loop {
+    let caps = reg.captures(&operation);
+    if caps.is_none() {
+      break;
+    }
+    let caps = caps.unwrap();
+    let cap_expresion = caps.get(0).unwrap().as_str();
+    let left_value : i32 = caps[1].parse().unwrap();
+    let right_value : i32 = caps[2].parse().unwrap();
+    let result: i32 = match operator {
+      "\\+" => left_value + right_value,
+      "\\*" => left_value * right_value,
+      "-" => left_value - right_value,
+      "/" => left_value / right_value,
+      _ => 0,        
+    };
+    operation = operation.replace(cap_expresion, &result.to_string());
+    
+  }
+  return operation;
+}
+
+fn main() {
+  println!("Por favor introduce tu expresion: ");
+  let mut expresion: String = String::new();
+  std::io::stdin().read_line(&mut expresion).unwrap();
+
+  // Regex
+  let operations = vec!["/", "\\*", "\\+", "-"];
+  let mut result: String = expresion;
+  for operator in operations {
+    let regex_str = format!("(\\d+)\\s*{operator}\\s*(\\d+)");
+    let re = Regex::new(&regex_str).unwrap();
+    result = math_operation(re, result, operator);
+  }
+
+  println!("Resultado: {result}");
+
+}
+
